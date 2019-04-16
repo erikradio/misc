1b3491513122657d37a941b06d5c8f21a6f37c00
erik radio <radio.erik@gmail.com>
2019-04-16 14:04:19 -0600

    add FAST lookup

--- geo2mods.py
+++ geo2mods.py
@@ -1,8 +1,9 @@
 import sys, re, uuid
 import xml.etree.ElementTree as ET
 from xml.etree.ElementTree import Element, SubElement, Comment, tostring
-import copy
-from datetime import datetime
+import copy, time, datetime
+import datetime, requests, json
+from fuzzywuzzy import fuzz, process
 
 def getGeoMetadata(infile_path):
     tree=ET.parse(infile_path)
@@ -13,13 +14,16 @@ def getGeoMetadata(infile_path):
     # print(title)
     doc['filename'] = infile_path
     doc['title'] = title
-
+    software = root.find('idinfo/native').text
+    doc['software'] = software
     issueDate = root.find('idinfo/citation/citeinfo/pubdate').text
     doc['issueDate'] = issueDate
     source = root.find('idinfo/citation/citeinfo/onlink').text
     doc['source'] = source
     publisher = root.find('idinfo/citation/citeinfo/origin').text
     doc['publisher'] = publisher
+    creator = root.find('dataIdInfo/idCredit').text
+    doc['creator'] = creator
     abstract = root.find('idinfo/descript/abstract').text
     purpose = root.find('idinfo/descript/purpose').text
     suppl = root.find('idinfo/descript/supplinf').text
@@ -38,6 +42,11 @@ def getGeoMetadata(infile_path):
     for x in keywords:
         key = x.text
         doc['topics'].append(key)
+
+
+
+
+
     doc['places'] = []
     places = root.findall('idinfo/keywords/place/placekey')
     for x in places:
@@ -63,6 +72,8 @@ def getGeoMetadata(infile_path):
     return doc
 
 def makeMods(doc):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d')
 
     root = Element('mods:mods')
 
@@ -78,11 +89,47 @@ def makeMods(doc):
     title = SubElement(titleInfo, 'mods:title')
     title.text = doc['title']
 
+    pub = SubElement(root, 'mods:name')
+    pubTerm = SubElement(pub, 'mods:namePart')
+    pubTerm.text = doc['publisher']
+    pubRole=SubElement(pub,'mods:role')
+    pubRoleTerm = SubElement(pubRole,'mods:roleTerm')
+    pubRoleTerm.set('type','text')
+    pubRoleTerm.text = 'publisher'
+    ##################this doesnt work very well!
+    # publisher = doc['publisher']
+    # url='http://fast.oclc.org/searchfast/fastsuggest?query='+publisher+'&queryIndex=suggest10&queryReturn=suggestall,idroot,auth&suggest=autoSubject&rows=20'
+    #
+    # r = requests.get(url)
+    # res = json.loads(r.text)
+    # jsonDocs=res['response']['docs']
+    # for x in jsonDocs:
+    #     suggestLower = x['suggestall'][0].lower()
+    #     fastID = x['idroot']
+    #
+    #     score=fuzz.token_sort_ratio(publisher,suggestLower)
+    #     if score > 80:
+    #         pub.set('authority', 'fast')
+    #         pubTerm.text = x['suggestall'][0]
+    #         newFast = fastID.replace('fst','')
+    #         pub.set('authorityURI','http://id.worldcat.org/fast/'+newFast)
+
+
+
+
+    creator = SubElement(root, 'mods:name')
+    creatorTerm = SubElement(creator, 'mods:namePart')
+    creatorTerm.text = doc['creator']
+    creatorRole=SubElement(creator,'mods:role')
+    creatorRoleTerm = SubElement(creatorRole,'mods:roleTerm')
+    creatorRoleTerm.set('type','text')
+    creatorRoleTerm.text = 'creator'
 
     type1 = SubElement(root, 'mods:typeOfResource')
     type1.text = 'cartographic'
     type2 = SubElement(root, 'mods:typeOfResource')
     type2.text = 'software, multimedia'
+    access = SubElement(root,'mods:accessCondition')
 
     originInfo = SubElement(root, 'mods:originInfo')
 
@@ -112,14 +159,50 @@ def makeMods(doc):
         subject = SubElement(root,'mods:subject')
         topic = SubElement(subject, 'mods:topic')
         topic.text = term
+        term= term.lower()
+        url='http://fast.oclc.org/searchfast/fastsuggest?query='+term+'&queryIndex=suggestall&queryReturn=suggestall,idroot,auth,tag,type,raw,breaker,indicator&suggest=autoSubject&rows=20'
+
+        r = requests.get(url)
+        res = json.loads(r.text)
+        jsonDocs=res['response']['docs']
+        # print(docs)
+        for x in jsonDocs:
+            suggestLower = x['suggestall'][0].lower()
+            fastID = x['idroot']
+
+            score=fuzz.token_sort_ratio(term,suggestLower)
+            if score > 80:
+                subject.set('authority', 'fast')
+                topic.text = x['suggestall'][0]
+                newFast = fastID.replace('fst','')
+                subject.set('authorityURI','http://id.worldcat.org/fast/'+newFast)
+
 
     for place in doc['places']:
         subject = SubElement(root,'mods:subject')
         placeTerm = SubElement(subject, 'mods:geographic')
-        placeTerm.text = place
+        # placeTerm.text = place
+        place = place.lower()
+        url='http://fast.oclc.org/searchfast/fastsuggest?query='+place+'&queryIndex=suggest51&queryReturn=suggestall,idroot,auth,tag,type,raw,breaker,indicator&suggest=autoSubject&rows=20'
+
+        r = requests.get(url)
+        res = json.loads(r.text)
+        jsonDocs=res['response']['docs']
+        # print(docs)
+        for x in jsonDocs:
+            suggestLower = x['suggestall'][0].lower()
+            fastID = x['idroot']
+
+            score=fuzz.token_sort_ratio(place,suggestLower)
+            if score > 80:
+                subject.set('authority', 'fast')
+                placeTerm.text = x['suggestall'][0]
+                newFast = fastID.replace('fst','')
+                subject.set('authorityURI','http://id.worldcat.org/fast/'+newFast)
+
 
     latlong = SubElement(root, 'mods:subject')
-    carto = SubElement(latlong,'mods:cartographic')
+    carto = SubElement(latlong,'mods:cartographics')
     cartoCoord = SubElement(carto, 'mods:coordinates')
     cartoCoord.text = doc['coordinates']
 
@@ -134,6 +217,8 @@ def makeMods(doc):
     form.set('authority', 'LCGFT')
     form.set('authorityURI','http://id.loc.gov/authorities/genreForms/gf2011026721')
     form.text = 'Vector data'
+    digNote = SubElement(physDesc,'mods:note')
+    digNote.text = doc['software']
 
     identifier = SubElement(root,'mods:identifier')
     identifier.set('type','local')
@@ -161,22 +246,24 @@ def makeMods(doc):
     relatedTitleInfo = SubElement(relatedItem,'mods:titleInfo')
     relatedTitle = SubElement(relatedTitleInfo,'mods:title')
     relatedTitle.text = doc['title']
+
+
+
+    recordInfo = SubElement(root,'mods:recordInfo')
+    recordCreationDate = SubElement(recordInfo,'mods:recordCreationDate')
+    recordCreationDate.set('encoding','w3cdtf')
+    recordCreationDate.text = st
+    recordOrigin = SubElement(recordInfo,'mods:recordOrigin')
+    recordOrigin.text = 'Record created using source metadata and additional metadata provided by the University of Colorado Boulder Libraries.'
+    recordSource = SubElement(recordInfo,'mods:recordContentSource')
+    recordSource.text = 'University of Colorado Boulder Libraries'
     tree = ET.ElementTree(root)
-    
+
 
     return tree
-#
-#         relatedTitle.text = row['relatedItem']
-#
-#         recordInfo = SubElement(root,'mods:recordInfo')
-#         recordCreationDate = SubElement(recordInfo,'mods:recordCreationDate')
-#         recordCreationDate.set('encoding','w3cdtf')
-#         recordCreationDate.text = st
-#         recordOrigin = SubElement(recordInfo,'mods:recordOrigin')
-#         recordOrigin.text = 'Manually created by Trent Purdy. Generated into xml using a python script by Erik Radio.'
-#         recordSource = SubElement(recordInfo,'mods:recordContentSource')
-#         recordSource.text = 'University of Colorado Boulder Libraries'
-#         tree.write(identifier.text + '.xml', xml_declaration=True, encoding="UTF-8")
+
+
+
 
 def main():
     infile_path = sys.argv[1]
