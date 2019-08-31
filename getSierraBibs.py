236b908f16a907257a44cc58fdded1803ef5f9f4
erik radio <radio.erik@gmail.com>
2019-08-30 18:31:24 -0600

    Update getSierraBibs.py

--- getSierraBibs.py
+++ getSierraBibs.py
@@ -7,7 +7,7 @@ secret = ''
 
 #https://libraries.colorado.edu:443/iii/sierra-api/v5/bibs/10252291/marc
 
-with open(sys.argv[1],'rU') as csvFile, open('sampleJson.json','w') as outfile:
+with open(sys.argv[1],'rU') as csvFile, open('bibs.csv','w') as outfile:
 
     # writer = csv.DictWriter(outcsv, fieldnames = ["filename", "original_term", "fast_term", "fast_id", "score"])
     # writer.writeheader()
@@ -22,7 +22,12 @@ with open(sys.argv[1],'rU') as csvFile, open('sampleJson.json','w') as outfile:
     # get auth token out of the response
     s.headers["Authorization"] = "Bearer {}".format(bearer_token)
     # s.get( # now that is in the session headers - so transparently on all other requests made with this session object
-    output = []
+    bibData = {}
+    fieldnames = ['id','language','title','author','materialType','publishYear','country']
+    w = csv.DictWriter(outfile, fieldnames)
+    w.writeheader()
+
+    # {'id': '4138916', 'updatedDate': '2014-11-25T17:13:37Z', 'createdDate': '2007-01-19T23:21:00Z', 'deleted': False, 'suppressed': False, 'lang': {'code': 'eng', 'name': 'English'}, 'title': 'Insurance maps of Cripple Creek, Teller Co., Colorado', 'author': 'Sanborn Map Company', 'materialType': {'code': 'e  ', 'value': 'Maps'}, 'bibLevel': {'code': 'o', 'value': 'ORIGINAL'}, 'publishYear': 1909, 'catalogDate': '2007-01-19', 'country': {'code': 'nyu', 'name': 'New York'}}
     for row in reader:
         # print(row)
         # print(row['PermaLink'])
@@ -33,23 +38,19 @@ with open(sys.argv[1],'rU') as csvFile, open('sampleJson.json','w') as outfile:
         url='https://libraries.colorado.edu:443/iii/sierra-api/v5/bibs/'+bib
         r = s.get(url)
         rjson = r.json()
-        output.append(rjson)
-        print(output)
-    # test='http://fast.oclc.org/searchfast/fastsuggest?&query=hog&queryIndex=suggestall&queryReturn=suggestall,idroot,auth,tag,type,raw,breaker,indicator&suggest=autoSubject&rows=10'
-    #http://fast.oclc.org/searchfast/fastsuggest?query=dog&queryIndex=suggestall@queryReturn=suggestall,idroot,auth,tag,type,raw,breaker,indicator&suggest=autoSubject&rows=3&callback=testcall
-    # terms=['denver']
-    # for term in terms:
-    #     url='http://fast.oclc.org/searchfast/fastsuggest?query='+term+'&queryIndex=suggestall&queryReturn=suggestall,idroot,auth,tag,type,raw,breaker,indicator&suggest=autoSubject&rows=20'
-    #
-    #     r = requests.get(url)
-    #     res = json.loads(r.text)
-    #     docs=res['response']['docs']
-    #     # print(docs)
-    #     for x in docs:
-    #         suggest = x['suggestall'][0].lower()
-    #         fastID = x['idroot']
-    #
-    #         score=fuzz.token_sort_ratio(term,suggest)
-    #         print(score)
-    #         writer.writerow({'filename': 'test', 'original_term': term, 'fast_term': suggest, 'fast_id': fastID, 'score':score})
-                # print(fuzz.ratio(term,suggest))
+
+        bibData['id'] = bib
+        # bibData['createdDate'] = rjson['createdDate']
+        if 'lang' in rjson:
+            bibData['language'] = rjson['lang']['name']
+        if 'title' in rjson:
+            bibData['title'] = rjson['title']
+        if 'author' in rjson:
+            bibData['author'] = rjson['author']
+        if 'materialType' in rjson:
+            bibData['materialType'] = rjson['materialType']['value']
+        if 'publishYear' in rjson:
+            bibData['publishYear'] = rjson['publishYear']
+        if 'country' in rjson:
+            bibData['country'] = rjson['country']['name']
+        w.writerow(bibData)
